83cc57f481f56d4c8888ffe9f6b65550878973bd
Felipe Fernandes <ripcorp@gmail.com>
2016-02-19 05:31:28 -0200

    Atualizações add grunt + bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,26 @@ module.exports = function(grunt) {
      // configurações das tasks
      less: {
        'assets/css/main.css': 'builds/css-less/main.less'
-     },
+     }, //less
+
      ngAnnotate: {
           options: {
               singleQuotes: true
           },
           app: {
               files: {
-                  'assets/main.js': ['builds/js/main.js']
+                  'assets/js/main.js': ['builds/js/main.js']
               }
           }
-      },
+      }, //ngAnnotate
+
       uglify: {
           js: { //target
               src: ['builds/js/main.js'],
               dest: 'assets/js/main.js'
           }
-      },
+      }, //uglify
+
       cssmin: {
         options: {
           shorthandCompacting: false,
@@ -30,16 +33,45 @@ module.exports = function(grunt) {
             'assets/css/main.css': 'assets/css/main.css'
           }
         }
-      }
+      }, //cssmin
+
+      concat: {
+        dist: {
+            src: ["builds/js/**/*",
+                  "builds/angular/angular.min.js",
+                  "builds/angular-auto-validate/dist/jcs-autovalidate.min.js",
+                  "builds/bootstrap/dist/js/bootstrap.min.js",
+                  "builds/jquery/dist/jquery.min.js",
+                  "builds/less/dist/less.min.js"]
+          , dest: "assets/js/main.js"
+        }
+      },
+
+      watch : {
+        dist : {
+          files : [
+            'builds/js/**/*',
+            'builds/css-less/**/*'
+          ],
+
+          tasks : [ 'concat', 'less', 'cssmin', 'ngAnnotate', 'uglify' ]
+        }
+      } // watch
 
 
   });
 
   // carrega plugins
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-  grunt.registerTask('default', ['less', 'cssmin', 'ngAnnotate', 'uglify']);
+  // Tarefa para o comando default
+  grunt.registerTask('default', ['concat', 'less', 'cssmin', 'ngAnnotate', 'uglify']);
+
+  // Tarefa para Watch
+  grunt.registerTask( 'w', [ 'watch' ] );
 };
--- README.md
+++ README.md
@@ -6,25 +6,18 @@ Arquivos iniciais de um projeto de FrontEnd (LESS + Twitter Bootstrap)
 
 	/index.html
 	/favicon.ico
-    	/css
-    	/css/bootstrap-responsive.css
-    	/css/bootstrap-responsive.min.css
-    	/css/bootstrap.css
-    	/css/bootstrap.min.css
-    	/css/main.css
-			
-		/img
-			/glyphicons-halflings-white.png
-			/glyphicons-halflings.png
-		
-		/js
-      	/js/less.min.js
-		/js/main.js		
-		/js/vendor
-		/js/vendor/bootstrap.min.js
-		/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js
-		/js/vendor/jquery-1.9.1.min.js
-        /js/vendor/bootstrap.js
+		/assets
+			/img
+			/js
+			/css			
+			/main.js		
 
-		/less
-		/less/main.less
+		/builds (não versionado)  	
+			/js
+			/less
+			/<todos scripts instalados>
+
+		/node_modules (não versionado)
+
+		Gruntfile.js (arquivo de configuração de scripts Grunt)
+		package.json (arquivo de configuração Grunt)
